b90b42232da0383983e7e632b5b58a457e66dfea
Parker Mathis <parkerjmathis@gmail.com>
2023-01-12 20:30:16 -0800

    Added signup, login, logout processes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,8 +1,63 @@
 const router = require("express").Router();
+const { User } = require("../../models");
 
+router.post("/", async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    console.log(dbUserData);
+    req.session.save(() => {
+      req.session.logged_in = true;
+      
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
+router.post("/login", async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
 
+    if (!dbUserData) {
+      res.status(400).json({ message: "Incorrect credentials." });
+      return;
+    }
 
+    const validPassword = await dbUserData.checkPassword(req.body.password);
 
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect credentials." });
+      return;
+    }
 
+    req.session.save(() => {
+      req.session.logged_in = true;
+      req.session.user_id = dbUserData.id;
+      res.status(200).json({ user: dbUserData, message: "Success!" });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 module.exports = router;
