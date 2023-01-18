77c67267941b24ef5a35904fb1e9ea43e7f4d371
Parker Mathis <parkerjmathis@gmail.com>
2023-01-17 18:35:46 -0800

    Added process to create a Hero

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const { User, Hero } = require("../models");
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   res.render("homepage", {
@@ -9,6 +10,10 @@ router.get("/", async (req, res) => {
 
 router.get("/login", (req, res) => {
   res.render("login");
+});
+
+router.get("/create", withAuth, async (req, res) => {
+  res.render("hero-create", { logged_in: true });
 });
 
 module.exports = router;
