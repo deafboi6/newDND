f8a8b85a14e73239624375074e3a90a7557f4a9b
Ravi Nagi <rasvindra@hotmail.com>
2023-01-19 18:56:39 -0800

    stashing game JS file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const heroRoutes = require("./heroRoutes");
+const monsterRoutes = require("./monster_routes")
 
 router.use("/users", userRoutes);
 router.use("/heroes", heroRoutes);
+router.use("/monster", monsterRoutes);
 
 module.exports = router;
