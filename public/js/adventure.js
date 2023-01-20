128e2fcf1330d720028dd3d00ae54c2e56f4c791
Parker Mathis <parkerjmathis@gmail.com>
2023-01-19 20:57:10 -0800

    stash

--- public/js/adventure.js
+++ public/js/adventure.js
@@ -9,6 +9,15 @@ const answerThreeEl = document.querySelector("#choice-three");
 const answerFourEl = document.querySelector("#choice-four");
 const mapEl = document.querySelector("#dungeon-map");
 
+// Hooks to the UI
+const answerButtonsEl = document.querySelector(".choiceButtons");
+const questionTextEl = document.querySelector("#question-text");
+const answerOneEl = document.querySelector("#choice-one");
+const answerTwoEl = document.querySelector("#choice-two");
+const answerThreeEl = document.querySelector("#choice-three");
+const answerFourEl = document.querySelector("#choice-four");
+const mapEl = document.querySelector("#dungeon-map");
+
 const Challenge = [5, 10, 15];
 const x = Math.floor(Math.random() * 3);
 var API = "https://www.dnd5eapi.co/api/monsters/?challenge_rating=" + Challenge[x];
